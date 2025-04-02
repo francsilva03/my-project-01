1a94ac28a880fadbdbdaa1d31cad031f76c34760
Francisco <francsilva03@gmail.com>
2025-04-02 17:42:45 +0200

    feat: segunda subida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
+/**
+ * @component AppComponent
+ * This is the root component of the application.
+ * It serves as the entry point for the Angular application.
+ */
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  /**
+   * @property {string} title - The title of the application.
+   */
   title = 'my-app';
+
+  /**
+   * @property {string} unusedVariable - A variable that is not used (intentional for linting test).
+   */
+
+  /**
+   * @constructor
+   * Initializes the AppComponent and logs a message to the console.
+   * Contains intentional linting issues for testing purposes.
+   */
+
+  onChange() {
+    console.log('change');
+  }
 }
